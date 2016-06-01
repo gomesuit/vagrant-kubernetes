fa08509a9d3e2a1fc5127a1d7c8499c422e2908f
gomesuit <gomesuit@gmail.com>
2016-06-01 23:01:31 +0900

    fix hosts

--- set-hosts.sh
+++ set-hosts.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
+echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
 echo "192.168.33.10 master01" >> /etc/hosts
 echo "192.168.33.20 node01" >> /etc/hosts
 echo "192.168.33.30 node02" >> /etc/hosts
