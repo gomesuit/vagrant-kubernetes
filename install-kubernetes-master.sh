ba99954fd94d9170b2ad160751f5a397dfd1c59d
gomesuit <gomesuit@gmail.com>
2016-06-04 16:09:53 +0900

    eth0 -> eth1

--- install-kubernetes-master.sh
+++ install-kubernetes-master.sh
@@ -24,7 +24,7 @@ EOF
 cat <<EOF > /etc/sysconfig/flanneld
 FLANNEL_ETCD="http://master01:4001"
 FLANNEL_ETCD_KEY="/atomic.io/network"
-FLANNEL_OPTIONS="eth0"
+FLANNEL_OPTIONS="eth1"
 EOF
 
 systemctl start etcd
