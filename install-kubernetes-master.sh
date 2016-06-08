66c5f04bbc3995046c7f4a81a3542cec9ea32f73
gomesuit <gomesuit@gmail.com>
2016-06-09 01:39:37 +0900

    Refactoring

--- install-kubernetes-master.sh
+++ install-kubernetes-master.sh
@@ -4,6 +4,13 @@ yum install -y kubernetes
 
 openssl genrsa -out /etc/kubernetes/serviceaccount.key 2048
 
+cat <<EOF > /etc/kubernetes/config
+KUBE_LOGTOSTDERR="--logtostderr=true"
+KUBE_LOG_LEVEL="--v=10"
+KUBE_ALLOW_PRIV="--allow-privileged=false"
+KUBE_MASTER="--master=http://master01:8080"
+EOF
+
 cat <<EOF > /etc/kubernetes/apiserver
 KUBE_API_ADDRESS="--insecure-bind-address=0.0.0.0"
 KUBE_ETCD_SERVERS="--etcd-servers=http://master01:2379"
@@ -17,19 +24,10 @@ EOF
 
 cat <<EOF > /etc/kubernetes/controller-manager
 #KUBE_CONTROLLER_MANAGER_ARGS="--service_account_private_key_file=/etc/kubernetes/serviceaccount.key"
-KUBELET_ADDRESSES="--machines=node01,node02"
-EOF
-
-cat <<EOF > /etc/kubernetes/config
-KUBE_LOGTOSTDERR="--logtostderr=true"
-KUBE_LOG_LEVEL="--v=0"
-KUBE_ALLOW_PRIV="--allow-privileged=false"
-KUBE_MASTER="--master=http://master01:8080"
-#KUBE_ETCD_SERVERS="--etcd_servers=http://master01:2379"
 EOF
 
 cat <<EOF > /etc/kubernetes/proxy
-KUBE_PROXY_ARGS="--master=http://master01:8080 --proxy-mode=iptables"
+KUBE_PROXY_ARGS="--proxy-mode=iptables"
 EOF
 
 systemctl start kube-apiserver
