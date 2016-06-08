0703f776a4a85c51204c1e12a0bf4f0d648d8794
gomesuit <gomesuit@gmail.com>
2016-06-08 18:40:18 +0900

    kubernetes-dashboard

--- install-kubernetes-master.sh
+++ install-kubernetes-master.sh
@@ -9,12 +9,14 @@ KUBE_API_ADDRESS="--insecure-bind-address=0.0.0.0"
 KUBE_ETCD_SERVERS="--etcd-servers=http://master01:2379"
 KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
 #KUBE_SERVICE_ADDRESSES="--portal_net=172.16.0.0/16"
-KUBE_ADMISSION_CONTROL="--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota"
-KUBE_API_ARGS="--service_account_key_file=/etc/kubernetes/serviceaccount.key"
+#KUBE_ADMISSION_CONTROL="--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota"
+KUBE_ADMISSION_CONTROL="--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ResourceQuota"
+#KUBE_API_ARGS="--service_account_key_file=/etc/kubernetes/serviceaccount.key"
+KUBE_API_ARGS="--secure-port=0"
 EOF
 
 cat <<EOF > /etc/kubernetes/controller-manager
-KUBE_CONTROLLER_MANAGER_ARGS="--service_account_private_key_file=/etc/kubernetes/serviceaccount.key"
+#KUBE_CONTROLLER_MANAGER_ARGS="--service_account_private_key_file=/etc/kubernetes/serviceaccount.key"
 KUBELET_ADDRESSES="--machines=node01,node02"
 EOF
 
@@ -46,3 +48,8 @@ kubectl config use-context default-context
 kubectl config set contexts.default-context.namespace default
 
 kubectl cluster-info
+
+
+# install kubernetes-dashboard
+kubectl create namespace kube-system
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
